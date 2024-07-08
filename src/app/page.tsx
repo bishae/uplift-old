4f18347783e6c6a050aaa9d22585c89ec2550fc3
Bisha <bisha.mail.drop@gmail.com>
2024-07-08 10:17:27 +0400

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main>
-      <h1>Home</h1>
-      <Button asChild>
-        <Link href="/dashboard">Get Started</Link>
-      </Button>
+    <main className="container flex h-screen max-w-[64rem] flex-col items-center justify-center gap-4 text-center">
+      <Link
+        href={siteConfig.links.twitter}
+        className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+        target="_blank"
+      >
+        Follow along on Twitter
+      </Link>
+      <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+        UPLIFT.
+      </h1>
+      <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+        (Description)
+      </p>
+      <div className="space-x-4">
+        <Link href="/dashboard" className={cn(buttonVariants({ size: "lg" }))}>
+          Get Started
+        </Link>
+        <Link
+          href={siteConfig.links.github}
+          target="_blank"
+          rel="noreferrer"
+          className={cn(buttonVariants({ variant: "outline", size: "lg" }))}
+        >
+          GitHub
+        </Link>
+      </div>
     </main>
   );
 }
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Uplift",
+  description: "",
+  url: "",
+  ogImage: "",
+  links: {
+    twitter: "",
+    github: "",
+  },
+};
